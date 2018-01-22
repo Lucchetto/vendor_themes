cbc2bba878b9a2bbd26111a642486840b3f5db9b
Alex Cruz <du.alexcruz@gmail.com>
2018-01-22 18:41:33 -0500

    Change makefile to compile as a regular apk

--- primary/dark/SettingsDarkTheme/Android.mk
+++ primary/dark/SettingsDarkTheme/Android.mk
@@ -5,6 +5,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SDK_VERSION := current
 LOCAL_PRIVILEGED_MODULE := false
 LOCAL_CERTIFICATE := platform
-LOCAL_PACKAGE_NAME := SystemDarkTheme
+LOCAL_PACKAGE_NAME := SettingsDarkTheme
 
 include $(BUILD_PACKAGE)
